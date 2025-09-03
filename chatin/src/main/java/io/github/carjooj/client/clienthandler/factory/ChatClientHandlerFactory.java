0ad6c70f5a5f853c14d6250c64523106371499e3
carjooj <victor@4info.com.br>
2025-09-03 19:20:14 -0300

    feat: injeção do logger na factory e nova exceção

--- chatin/src/main/java/io/github/carjooj/client/clienthandler/factory/ChatClientHandlerFactory.java
+++ chatin/src/main/java/io/github/carjooj/client/clienthandler/factory/ChatClientHandlerFactory.java
@@ -4,26 +4,31 @@ import io.github.carjooj.client.Client;
 import io.github.carjooj.client.SocketClient;
 import io.github.carjooj.client.clienthandler.ClientHandler;
 import io.github.carjooj.client.clientregistry.ClientRegistry;
+import io.github.carjooj.exceptions.ClientConnectionException;
+import io.github.carjooj.exceptions.HandlerCreationException;
+import io.github.carjooj.logger.AppLogger;
 
-import java.io.IOException;
 import java.net.Socket;
 
 public class ChatClientHandlerFactory implements ClientHandlerFactory {
 
     private final ClientRegistry clientRegistry;
 
-    public ChatClientHandlerFactory(ClientRegistry clientRegistry) {
+    private final AppLogger logger;
+
+    public ChatClientHandlerFactory(ClientRegistry clientRegistry, AppLogger logger) {
         this.clientRegistry = clientRegistry;
+        this.logger = logger;
     }
 
     @Override
-    public Runnable create(Socket clientSocket) {
+    public Runnable create(Socket clientSocket) throws HandlerCreationException {
         try {
             Client client = new SocketClient(clientSocket);
 
-            return new ClientHandler(client, clientSocket, clientRegistry);
-        } catch (IOException e) {
-            throw new RuntimeException("Não foi possível criar ClientHandler", e);
+            return new ClientHandler(client, clientSocket, clientRegistry, logger);
+        } catch (ClientConnectionException e) {
+            throw new HandlerCreationException("Não foi possível criar ClientHandler", e);
         }
 
 
