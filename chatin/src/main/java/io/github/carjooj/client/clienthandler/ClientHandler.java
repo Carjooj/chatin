cab7ef2f61c515869c9f728aaff7158f7df3ecd2
carjooj <victor@4info.com.br>
2025-09-03 19:23:48 -0300

    feat: adicionando logger e adequando run para nova exceção

--- chatin/src/main/java/io/github/carjooj/client/clienthandler/ClientHandler.java
+++ chatin/src/main/java/io/github/carjooj/client/clienthandler/ClientHandler.java
@@ -2,6 +2,7 @@ package io.github.carjooj.client.clienthandler;
 
 import io.github.carjooj.client.Client;
 import io.github.carjooj.client.clientregistry.ClientRegistry;
+import io.github.carjooj.logger.AppLogger;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -9,33 +10,35 @@ import java.io.InputStreamReader;
 import java.net.Socket;
 
 public class ClientHandler implements Runnable {
-
     private final Client client;
 
     private final Socket socket;
 
     private final ClientRegistry clientRegistry;
 
-    public ClientHandler(Client client, Socket clientSocket, ClientRegistry clientRegistry) {
+    private final AppLogger logger;
+
+    public ClientHandler(Client client, Socket clientSocket, ClientRegistry clientRegistry, AppLogger logger) {
         this.client = client;
         this.socket = clientSocket;
         this.clientRegistry = clientRegistry;
+        this.logger = logger;
     }
 
     public void run() {
         Socket clientSocket = this.socket;
+        try (
+                clientSocket;
+                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))
+        ) {
 
-        try (clientSocket) {
-
-            try (
-                    BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))
-            ) {
-                String username = in.readLine();
-                if (username == null || username.trim().isEmpty()) {
-                    return;
-                }
-                client.setUsername(username);
-                clientRegistry.add(client);
+            String username = in.readLine();
+            if (username == null || username.trim().isEmpty()) {
+                return;
+            }
+            client.setUsername(username);
+            clientRegistry.add(client);
+            try {
                 String messageFromClient;
                 while ((messageFromClient = in.readLine()) != null) {
                     if ("\\quit".equals(messageFromClient)) {
@@ -47,9 +50,7 @@ public class ClientHandler implements Runnable {
                 clientRegistry.remove(client);
             }
         } catch (IOException e) {
-            e.printStackTrace();
+            logger.error("Erro na comunicação com o cliente: ", e);
         }
-
-
     }
 }
