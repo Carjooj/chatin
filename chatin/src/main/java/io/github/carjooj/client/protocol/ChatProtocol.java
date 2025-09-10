557bac4d5953b9a581b40015423d0a7a232bc5fb
carjooj <victor@4info.com.br>
2025-09-10 15:08:39 -0300

    feat: Criando classe para acesso do delimitador do novo protocolo

---
+++ chatin/src/main/java/io/github/carjooj/client/protocol/ChatProtocol.java
@@ -0,0 +1,10 @@
+package io.github.carjooj.client.protocol;
+
+public final class ChatProtocol {
+
+    public static final String MESSAGE_DELIMITER = "<<EOF>>";
+
+    private ChatProtocol() {
+    }
+
+}
