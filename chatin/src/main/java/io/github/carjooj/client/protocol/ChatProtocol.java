54804199dffc09813022392908ef115535d83f90
carjooj <victor@4info.com.br>
2025-09-24 17:14:13 -0300

    feat: Criando padrões de formato para mensagens e notificações

--- chatin/src/main/java/io/github/carjooj/client/protocol/ChatProtocol.java
+++ chatin/src/main/java/io/github/carjooj/client/protocol/ChatProtocol.java
@@ -4,6 +4,12 @@ public final class ChatProtocol {
 
     public static final String MESSAGE_DELIMITER = "<<EOF>>";
 
+    public static final String USER_MESSAGE_FORMAT = "[%s]: %s";
+
+    public static final String SERVER_NOTIFICATION_FORMAT = "[SERVER]: %s";
+
+    public static final String SERVER_JOIN_NOTIFICATION_FORMAT = String.format(SERVER_NOTIFICATION_FORMAT, "%s entrou na sala");
+
     private ChatProtocol() {
     }
 
